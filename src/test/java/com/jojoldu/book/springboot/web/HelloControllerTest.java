20652f9e1e5432f48b7c00bf84a7fbd3d21e674d
devjjongs <whdtjr5689@gmail.com>
2021-08-05 20:49:08 +0900

    add class file modified gradle version

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -9,9 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)//스프링 실행자로, 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 함
 @WebMvcTest(controllers = HelloController.class)
@@ -25,7 +25,21 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello"))//MockMvc를 통해 /hello 주소로 HTTP HET 요청
-        .andExpect(status().isOk())//mvc.perform의 결과를 검증 / 200,404,500등의 상태를 검증 그 중 200을 검증
-        .andExpect(content().string(hello));
+                .andExpect(status().isOk())//mvc.perform의 결과를 검증 / 200,404,500등의 상태를 검증 그 중 200을 검증
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                        // param : API 테스트할 때 사용될 요청 파라미터를 설정하며 값은 String만 허용(숫자/날짜 등의 데이터 등록시 문자열로 변경)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+                            //  jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드로, $를 기준으로 필드명을 명시
     }
 }
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -16,9 +16,9 @@ public class HelloResponseDtoTest {
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
         //then
-        assertThat(dto.getName()).isEqualTo(name);
-        assertThat(dto.getAmount()).isEqualTo(amount);
+        assertThat(dto.getName()).isEqualTo(name);        assertThat(dto.getAmount()).isEqualTo(amount);
         //assertThat : 테스트 검증 라이브러리의 검증 메소드 / 검증하고 싶은 대상을 메소드 인자로 받음 / 메소드 체이닝 지원 -> isEqualTo와 같이 메소드 이어서 사용 가능
         //isEqualTo : assertj의 동등 비교 메소드 / assertThat에 있는 값과 isEqualTo의 값을 비교해서 같을 때만 성공
+        //assertj 역시 Junit에서 자동으로 라이브러리 등록
     }
 }
